a2ab154a2df4e1f4842e0845ef68e555df34d02a
Hannah <hecrook@gmail.com>
2024-07-05 15:11:54 +0100

    i think ive updated files but I'm not sure so commiting to see

--- s17/bin/subtractMaternalfromMatchedMutect2.sh
+++ s17/bin/subtractMaternalfromMatchedMutect2.sh
@@ -15,18 +15,21 @@ source activate bcftools
 ### SUBTRACTING MATERNAL HAPLOTYPECALLER VCF FROM TUMOUR MUTECT2 VCF ###
 
 ## S17 ##
+cd /data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/filterVCF/1anal/s17
 
 # Define inputs and outputs
-S17_mat='/data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/filterVCF/1anal/s17/m_S17.haplotypecaller.filtered.vcf.gz'
-S17_tum='/data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/filterVCF/1anal/s17/results/filteredHC/t_s17_filteredHC.mutect2.filtered.vcf'
-s17_child='/data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/filterVCF/1anal/s17/c_S17.haplotypecaller.filtered.vcf'
+S17_mat='input/m_S17.haplotypecaller.filtered.vcf.gz'
+S17_tum='results/filteredHC/t_s17_filteredHC.mutect2.filtered.vcf.gz'
+S17_child='input/c_S17.haplotypecaller.filtered.vcf.gz'
 
-S17_tum_minus_maternal='/data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/bcftools/tumour_somatic_VC/results/S17/germlinefilteredHC/S17_tumourMinusMaternal.vcf.gz'
-S17_tum_minus_germline='/data/scratch/DMP/UCEC/EVOLIMMU/hcrook/gestational_WES_WGS/analysis/bcftools/tumour_somatic_VC/results/S17/germlinefilteredHC/S17_tumourMinusGermline.vcf.gz'
+S17_germline='input/s17_maternalAndChildGermline.vcf.gz'
+S17_tum_minus_germline='results/germlinefilteredHC/S17_tumourMinusGermline.vcf.gz'
 
+# merge
+bcftools merge $S17_mat $S17_child -Oz -o $S17_germline
+
+# index
+bcftools index $S17_germline
 
 # Subtract maternal germline variants from tumour VCF
-bcftools isec -C $S17_tum $S17_mat -Oz -o $S17_tum_minus_maternal
-
-# subtract child germline variants from tumourminusmaternal VCF (all germline variants removed after this)
-bcftools isec -C $S17_tum_minus_maternal $ -Oz -o $S17_tum_minus_germline
+bcftools isec -C $S17_tum $S17_germline -Oz -o $S17_tum_minus_germline
